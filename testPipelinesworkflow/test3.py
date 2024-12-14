ba44ca4cd7ecac17a93cca7e32cf050e50705a19
DESKTOP-CNPPB85\PC-OSCAR <oepaezb@gmail.com>
2024-12-14 13:43:46 -0500

    actividad 2

--- testPipelinesworkflow/test3.py
+++ testPipelinesworkflow/test3.py
@@ -1,11 +1,21 @@
-from funciones import sumar
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
+from time import sleep
 
-class TestCalculaMedia(unittest.TestCase):
-    
-    def test_1(self):
-        resultado = sumar(2, 4, 5 , 1)
-        self.assertNotEqual(2, resultado)
+class eis_test(unittest.TestCase):
+
+	def setUp(self):
+		self.browser = webdriver.Chrome()
+
+	def test_load(self):
+		browser = self.browser
+		browser.get("https://campusvirtualunillanos.co/")
+		self.assertIn("Google", browser.title, msg="Fallo en la comparacion de titulos")
+		
+
+	def tearDown(self):
+		self.browser.quit()
 
 if __name__ == '__main__':
-    unittest.main()
+	unittest.main()
