d3e37949209475cf5310d1160a5bc22c1aee7d5e
DESKTOP-CNPPB85\PC-OSCAR <oepaezb@gmail.com>
2024-12-14 13:47:08 -0500

    actividad 2-sin verificar

--- testPipelinesworkflow/test2.py
+++ testPipelinesworkflow/test2.py
@@ -5,7 +5,7 @@ class TestCalculaMedia(unittest.TestCase):
     
     def test_1(self):
         resultado = sumar(2, 4, 5 , 1)
-        self.assertNotEqual(2, resultado)
+        self.assertEqual(12, resultado)
 
 if __name__ == '__main__':
     unittest.main()
